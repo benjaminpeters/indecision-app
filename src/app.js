02552f010795439c35367fce724923e728de9932
Benjamin <benpeters49@gmail.com>
2018-07-13 19:33:05 -0400

    Added JSX expressions

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 console.log("App.js is running");
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var template2 = (
+    <div>
+        <h1>Benjamin</h1>
+        <p>Age: 28</p>
+        <p>Location: TO</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
