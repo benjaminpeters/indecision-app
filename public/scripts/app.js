16ba389c3b9f84c74fd85444e405e495eadc5c70
Benjamin <benpeters49@gmail.com>
2018-07-13 21:31:54 -0400

    Added some conditional rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,8 @@ console.log("App.js is running");
 
 var app = {
     title: "Indecision App",
-    subTitle: "Put your life in the hands of a computer"
+    subTitle: "Put your life in the hands of a computer",
+    options: ['One', ' Two']
 
     // JSX - JavaScript XML
 };var template = React.createElement(
@@ -15,10 +16,15 @@ var app = {
         null,
         app.title
     ),
-    React.createElement(
+    app.subTitle && React.createElement(
         "p",
         null,
         app.subTitle
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.options && app.options.length > 0 ? "Here are your options: " + app.options : "No Options"
     ),
     React.createElement(
         "ol",
@@ -41,31 +47,31 @@ var user = {
     age: 28,
     location: "TO"
 };
-
-var userName = 'Ben';
-var userAge = 28;
-var userLocation = "TO";
-
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            "p",
+            null,
+            "Location: ",
+            location
+        );
+    }
+}
 var template2 = React.createElement(
     "div",
     null,
     React.createElement(
         "h1",
         null,
-        user.name
+        user.name ? user.name : 'Anonymous'
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         "p",
         null,
         "Age: ",
         user.age
     ),
-    React.createElement(
-        "p",
-        null,
-        "Location: ",
-        user.location
-    )
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ console.log("App.js is running");
 
 var app = {
     title: "Indecision App",
-    subTitle: "Put your life in the hands of a computer"
+    subTitle: "Put your life in the hands of a computer",
+    options: ['One',' Two']
 }
 
 // JSX - JavaScript XML
 var template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subTitle}</p>
+        {app.subTitle && <p>{app.subTitle}</p>}
+        <p>{app.options && app.options.length > 0 ? "Here are your options: " + app.options : "No Options" }</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
@@ -22,16 +24,16 @@ var user = {
     age: 28,
     location: "TO"
 };
-
-var userName = 'Ben'
-var userAge = 28;
-var userLocation = "TO"
-
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
 var template2 = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age  && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
