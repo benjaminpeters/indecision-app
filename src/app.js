8a2052055a560c2592aeadd97e24bcbf527d4b91
Benjamin <benpeters49@gmail.com>
2018-07-16 21:07:49 -0400

    handling events and methods

--- src/app.js
+++ src/app.js
@@ -27,22 +27,32 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick() {
+        alert('handlePick');
+    }
+
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         )
      }
 }
 
 class Options extends React.Component {
+
+    handleRemoveAll(){
+        alert('handleRemoveAll');
+    }
+
     render() {
         return (
             <div>
-                    {
-                        this.props.options.map((option) => <Option key={option} optionText={option}/>)
-                    }
+                <button onClick={this.handleRemoveAll}>Remove All</button>
+                {
+                    this.props.options.map((option) => <Option key={option} optionText={option}/>)
+                }
             </div>
         );
     }
@@ -58,11 +68,24 @@ class Option extends React.Component {
     }
 }
 
+
+
 class AddOption extends React.Component {
+    handleAddOption(e){
+        e.preventDefault();
+        const option = e.target.elements.option.value.trim();
+
+        if(option) {
+            alert(option);
+        }
+    }
     render() {
         return (
             <div>
-                <p>AddOption component here</p>
+                <form onSubmit={this.handleAddOption}>
+                    <input type='text' name='option' />
+                    <button>Add Option</button>
+                </form>
             </div>
         );
     }
