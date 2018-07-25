5b2ddc780b73d05e3b6c301ea4853637f341867c
Benjamin <benpeters49@gmail.com>
2018-07-25 18:25:07 -0400

    Working with ES6 import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log('app.js is running!');
+// import './utils.js';
+// import { square, add } from './utils.js';
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(100, 23));
+
+import { canDrink, isAdult } from './person.js';
+
+console.log(canDrink(5));
+console.log(canDrink(25));
+
+console.log(isAdult(18));
+console.log(isAdult(1));
