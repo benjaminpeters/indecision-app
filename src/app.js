0e08047ce8640af8b18864500f1c96bfe9a72fa1
Benjamin <benpeters49@gmail.com>
2018-07-31 20:37:09 -0400

    Added es6 class properties so don't have to bind methods because arrow funtions use this as their parent

--- src/app.js
+++ src/app.js
@@ -3,3 +3,30 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class OldSyntax {
+    constructor() {
+        this.name = 'Ben';
+        this.getGreeting = this.getGreeting.bind(this);
+    }
+
+    getGreeting() {
+        return `hi my name is ${this.name}.`
+    }
+}
+
+const oldSyntax = new OldSyntax();
+const getGreeting = oldSyntax.getGreeting;
+console.log(getGreeting());
+
+//----
+class NewSyntax {
+    name = 'Boop';
+    getGreeting = () => {
+        return `hi my name is ${this.name}.`;
+    }
+}
+
+const newSyntax = new NewSyntax();
+const newGetGreeting = newSyntax.getGreeting;
+console.log(newGetGreeting());
