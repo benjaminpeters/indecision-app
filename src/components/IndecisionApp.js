314e9a2f10c7ad884187191dec161c46a93de6af
Benjamin <benpeters49@gmail.com>
2018-08-02 22:34:10 -0400

    added style variables - colors, spacing

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -67,18 +67,20 @@ export default class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header subtitle={subtitle}/>
-                <Action 
-                    hasOptions={this.state.options.length > 0} 
-                    handlePick={this.handlePick}
-                />
-                <Options 
-                    options={this.state.options}
-                    handlDeleteOptions={this.handlDeleteOptions}
-                    handlDeleteOption={this.handleDeleteOption}
+                <div className="container">
+                    <Action 
+                        hasOptions={this.state.options.length > 0} 
+                        handlePick={this.handlePick}
                     />
-                <AddOption 
-                    handleAddOption={this.handleAddOption}
-                />
+                    <Options 
+                        options={this.state.options}
+                        handlDeleteOptions={this.handlDeleteOptions}
+                        handlDeleteOption={this.handleDeleteOption}
+                        />
+                    <AddOption 
+                        handleAddOption={this.handleAddOption}
+                    />
+                </div>
                 <OptionModal
                     selectedOption={this.state.selectedOption}
                     handleClearSelectedOption={this.handleClearSelectedOption}
